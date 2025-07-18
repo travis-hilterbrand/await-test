291c6a31ad4f0ad92ea4b84581d11dfdabe3febe
Travis Hilterbrand <travis.hilterbrand@zingly.ai>
2025-07-18 11:48:51 -0700

    await examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
 import "./App.css";
 
+const sleep = (ms: number, label: string): Promise<void> =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.info(`sleep end(${label})`);
+      resolve();
+    }, ms)
+  );
+
 function App() {
-  return <div>App</div>;
+  const awaitTest1 = async (): Promise<number> => {
+    console.info("awaitTest1 1");
+    return 1;
+  };
+  const awaitTest2 = async (): Promise<number> => {
+    console.info("awaitTest2 1");
+    await sleep(2000, "awaitTest2");
+    console.info("awaitTest2 2");
+    return 1;
+  };
+  const awaitTest3 = async (): Promise<number> => {
+    console.info("awaitTest3 1");
+    await sleep(2000, `awaitTest3 ${1}`);
+    console.info("awaitTest3 2");
+    sleep(2000, `awaitTest3 ${2}`);
+    return 1;
+  };
+
+  return (
+    <div>
+      App
+      <hr />
+      <button
+        onClick={async () => {
+          const response = await awaitTest1();
+          console.info(`onClick1(${response})`);
+        }}
+      >
+        Test 1
+      </button>
+      <button
+        onClick={async () => {
+          const response = await awaitTest2();
+          console.info(`onClick2(${response})`);
+        }}
+      >
+        Test 2
+      </button>
+      <button
+        onClick={async () => {
+          const response = await awaitTest3();
+          console.info(`onClick3(${response})`);
+        }}
+      >
+        Test 3
+      </button>
+    </div>
+  );
 }
 
 export default App;
